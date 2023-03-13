21b8df44155f682c16726a62cb4a58b8b96862f0
Panoj Kamolrattanawech <panoj.ka@gmail.com>
2023-03-13 15:41:20 +0700

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,54 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Landing from './routes/landing'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    // <div>
+    //   <p>
+    //     route
+    //   </p>
+    // </div>
+    <Router>
+    <div>
+      {/* <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav> */}
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/about">
+          <Landing />
+        </Route>
+        <Route path="/users">
+          <Landing />
+        </Route>
+        <Route path="/">
+          <Landing />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   )
 }
 
