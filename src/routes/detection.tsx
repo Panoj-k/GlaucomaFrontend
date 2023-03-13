7f6d29c8fb7901796184efc30af7c5775c73bb07
Panoj Kamolrattanawech <panoj.ka@gmail.com>
2023-03-13 21:06:47 +0700

    add route to detection page button

---
+++ src/routes/detection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Detection = () => {
+    return (
+        <div>
+            <h2>
+                Detection page
+            </h2>
+        </div>
+    )
+}
+
+export default Detection
