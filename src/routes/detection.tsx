7bc714f9e564d1f6a86ede1cde71fab2885a73ca
Panoj Kamolrattanawech <panoj.ka@gmail.com>
2023-03-14 20:59:48 +0700

    add dropbox area and ondrop function for single

--- src/routes/detection.tsx
+++ src/routes/detection.tsx
@@ -1,70 +1,88 @@
-import React, {useState} from 'react'
-import PageContainer from '../components/containers/PageContainer'
-
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import { Box, Container, Button } from '@mui/material';
+import React, { useState, useCallback } from "react";
+import PageContainer from "../components/containers/PageContainer";
+import { useDropzone } from "react-dropzone";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import { Box, Container, Button } from "@mui/material";
+import { color } from "@mui/system";
 
 const Detection = () => {
+  const [step, setStep] = useState(0);
 
-    const [step,setStep] = useState(0)
+  const steps = ["Upload", "Review", "Result"];
+  //---picture drop
+  const [imageUrl, setImageUrl] = useState("");
 
-    const steps = [
-        'Upload',
-        'Review',
-        'Result',
-      ];
-      
-    return (
-        <Container>
-            {/* <PageContainer> */}
-            <h2>
-                Detection page
-            </h2>
-               <Box sx={{display: 'grid',placeItems: 'center'}}>
-               <Stepper activeStep={step} alternativeLabel sx={{width: ['200px', '400px', '600px']}}>
-                    {steps.map((label) => (
-                        <Step key={label}>
-                        <StepLabel>{label}</StepLabel>
-                        </Step>
-                    ))}
-                </Stepper>
-               </Box>
-            <h6 >
-                Upload step: Please drag and drop pictures in the box below or click select pictures button to add pictures.<br />
-                Accepted File Types: .jpg, .png, .tif. Maximum picture at 20.
-            </h6>
-            <Box textAlign={'center'}>
-                <Button
-                    variant='contained'
-                    sx = {{ alignSelf: 'center'}}
-                >
-                    Select Pictures
-                </Button>
-            </Box>
-            
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const file = acceptedFiles[0];
+    const reader = new FileReader();
 
+    reader.onload = () => {
+      setImageUrl(reader.result as string);
+    };
 
-        </Container>
-    )
-    // return (
-    //     <div>
-    //         <PageContainer>
-    //             <Stepper activeStep={1} alternativeLabel sx={{width: ['200px', '400px', '600px']}}>
-    //                 {steps.map((label) => (
-    //                     <Step key={label}>
-    //                     <StepLabel>{label}</StepLabel>
-    //                     </Step>
-    //                 ))}
-    //             </Stepper>
+    reader.readAsDataURL(file);
 
-    //         </PageContainer>
-    //         <h2>
-    //             Detection page
-    //         </h2>
-    //     </div>
-    // )
-}
+    setStep(1);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
-export default Detection
+  return (
+    <Container>
+      {/* <PageContainer> */}
+      <h2>Detection page</h2>
+      <Box sx={{ display: "grid", placeItems: "center" }}>
+        <Stepper
+          activeStep={step}
+          alternativeLabel
+          sx={{ width: ["200px", "400px", "600px"] }}
+        >
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </Box>
+      <h6>
+        Upload step: Please drag and drop pictures in the box below or click
+        select pictures button to add pictures.
+        <br />
+        Accepted File Types: .jpg, .png, .tif. Maximum picture at 20.
+      </h6>
+      <Box
+        textAlign={"center"}
+        sx={{
+          bgcolor: "background.paper",
+          boxShadow: 1,
+          borderRadius: 2,
+          p: 2,
+          minWidth: 300,
+          marginTop: 4,
+        }}
+      >
+        <Button variant="contained" sx={{ alignSelf: "center" }}>
+          Select Pictures
+        </Button>
+        <Box>
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+              <p>Drop the picture here ...</p>
+            ) : (
+              <p>Drag and drop a picture here, or click to select a picture</p>
+            )}
+            {imageUrl && (
+              <div>
+                <img src={imageUrl} alt="Uploaded picture" />
+              </div>
+            )}
+          </div>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Detection;
