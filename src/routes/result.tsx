ac0087b7410152292f7d6e44eb3cbc039eb244f2
Panoj Kamolrattanawech <panoj.ka@gmail.com>
2023-03-18 13:05:46 +0700

    add stepper to result page

--- src/routes/result.tsx
+++ src/routes/result.tsx
@@ -1,10 +1,36 @@
-import react from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import PageContainer from "../components/containers/PageContainer";
+import { useDropzone } from "react-dropzone";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import { Box, Container, Button } from "@mui/material";
+import { color } from "@mui/system";
+import { useTranslation } from "react-i18next";
+import { Margin } from "@mui/icons-material";
+import { useHistory } from "react-router-dom";
 
 const Result = () => {
+  const steps = ["Upload", "Review", "Result"];
+  const { t } = useTranslation();
+
   return (
-    <div>
-      <p> show result here!</p>
-    </div>
+    <Container>
+      <h2>Detection result</h2>
+      <Box sx={{ display: "grid", placeItems: "center" }}>
+        <Stepper
+          activeStep={2}
+          alternativeLabel
+          sx={{ width: ["200px", "400px", "600px"] }}
+        >
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </Box>
+    </Container>
   );
 };
 
