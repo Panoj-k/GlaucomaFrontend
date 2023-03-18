6769821a28f44dadd97a040cc71efd6b882c4a1a
Panoj Kamolrattanawech <panoj.ka@gmail.com>
2023-03-18 14:46:19 +0700

    add image context provider

---
+++ src/context/imageContext.tsx
@@ -0,0 +1,29 @@
+import React, {
+  useState,
+  createContext,
+  SetStateAction,
+  Dispatch,
+} from "react";
+
+export const AppContext = createContext(null);
+
+export interface Image {
+  url: string;
+  name: string;
+}
+
+export interface ImageContextStruct {
+  images: Image[];
+  setImages: Dispatch<SetStateAction<Image[]>>;
+}
+
+export const ImageContext = createContext({} as ImageContextStruct);
+
+const ImageContextProvider = ({ ...props }) => {
+  const [images, setImages] = useState<Image[]>([]);
+  const value = { images, setImages };
+
+  return <ImageContext.Provider value={value} {...props} />;
+};
+
+export default ImageContextProvider;
