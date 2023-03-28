452d8934b509841e1c881db1316b0dd2c8830107
Panoj Kamolrattanawech <panoj.ka@gmail.com>
2023-03-28 16:24:26 +0700

    remove unused code from prediction model

--- src/model/PredictionModel.ts
+++ src/model/PredictionModel.ts
@@ -23,7 +23,7 @@ async function getPrediction() {
 }
 
 export async function PredictionModel(images: ImageInterface[]) {
-  // const model = await loadModel();
+  const model = await loadModel();
   imageList = [];
 
   var bar = new Promise((resolve, reject) => {
@@ -36,6 +36,8 @@ export async function PredictionModel(images: ImageInterface[]) {
       console.log("here");
       _imagefile.onload = async () => {
         console.log("here3");
+        console.log(model);
+        console.log(model.summary());
         if (imageElement && imageElement.src) {
           //@ts-ignore
           // imageElement.src = this.src
@@ -51,25 +53,32 @@ export async function PredictionModel(images: ImageInterface[]) {
 
           console.log("here2");
           //console.log(dst.rows, dst.cols)
-          // const input = tf.tensor(dst.data, [1,dst.rows, dst.cols, 3])
-          const input = tf.tensor(dst.data, [dst.rows, dst.cols, 3]);
-          console.log("line45");
-          imageList.push(input);
-          // const imageTensor = tf.reshape()
-          // console.log(input)
-          // const prediction = model.predict(input) as tf.Tensor
-          // console.log('prediction')
-          // console.log(prediction)
-          // const data = await prediction.data();
-          // console.log('data')
-          // console.log(data)
-          // console.log(data[0],data[1])
+          const input = tf.tensor(
+            dst.data,
+            [1, dst.rows, dst.cols, 3],
+            "float32"
+          );
+          //const input = tf.tensor(dst.data, [dst.rows, dst.cols, 3]);
+          // console.log("line45");
+          // imageList.push(input);
+          //const imageTensor = tf.reshape()
+          console.log(input);
+          const prediction = model.predict(input) as tf.Tensor;
+          console.log("prediction");
+          console.log(prediction);
+          const data = await prediction.data();
+          console.log("data");
+          console.log(data);
+          console.log(data[0], data[1]);
+          const result = [data[0], data[1]];
+          //console.log(model.output.shape); // e.g. [null, 10]
+          //console.log(model.output.dtype); // e.g. "float32"
         }
       };
       _imagefile.src = imageElement.src;
-      if (i === images.length - 1) {
-        getPrediction();
-      }
+      // if (i === images.length - 1) {
+      //   getPrediction();
+      // }
     });
   });
   // bar.then(() => {
@@ -79,79 +88,3 @@ export async function PredictionModel(images: ImageInterface[]) {
   //   console.log(predictionList);
   // });
 }
-
-// interface P {
-//   images: ImageInterface[];
-// }
-
-// const PredictionModel = ({ images }: P) => {
-//   // const PredictionModel = (images: ImageInterface[]) => {
-//   // const [imageList, setImageList] = useState<string[]>([]);
-//   const [model, setModel] = useState<LayersModel>();
-
-//   async function loadModel() {
-//     console.log("loading model...");
-//     const model = await loadLayersModel(window.origin + "/model_js/model.json");
-//     //const model = await loadLayersModel("model_js/model.json");
-//     //const model = await loadLayersModel("file:model_js/model.json");
-//     return model;
-//   }
-
-//   useEffect(() => {
-//     async function fn() {
-//       const _model = await loadModel();
-//       setModel(_model);
-//     }
-
-//     fn();
-//   }, []);
-
-//   //const model = loadModel();
-//   const inputList = [1, 2]; //delete this line
-
-//   console.log("prediction model is called");
-
-//   const resizeImage = (imageUrl: string) => {
-//     console.log("resizing image is called");
-//     const imageFile = cv.imread(imageUrl);
-//     const dst: Mat = new cv.Mat(imageFile.cols, imageFile.rows, cv.CV_8UC4);
-//     cv.cvtColor(imageFile, dst, cv.COLOR_BGR2RGB);
-//     cv.resize(imageFile, dst, new cv.Size(224, 224), 0, 0, cv.INTER_AREA);
-//     cv.normalize(imageFile, dst, 0, 255, cv.NORM_MINMAX);
-//     return imageFile;
-//   };
-
-//   //loop
-//   for (let i = 0; i < images.length; i++) {
-//     console.log(images[i].name);
-//     const imageFile = resizeImage(images[i].url);
-//     console.log(imageFile);
-//     // model.then((model) => {
-//     //   let result = model.predict(image);
-//     // });
-//     //let result = model.predict(inputTensor)
-//     //console.log(images[i].result);
-//   }
-//   // inputList =
-//   //   const [inputValue, setInputValue] = useState<number>(0);
-//   // const [outputValue, setOutputValue] = useState<number | null>(null);
-//   // const [model, setModel] = useState<tf.LayersModel | null>(null);
-
-//   // const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//   //   setInputValue(parseFloat(event.target.value));
-//   // };
-
-//   // const handleButtonClick = async () => {
-//   //   if (!model) {
-//   //     setModel(await loadModel());
-//   //   }
-//   //   const prediction = model.predict(
-//   //     tf.tensor2d([[inputValue]])
-//   //   ) as tf.Tensor<tf.Rank.R1>;
-//   //   setOutputValue(prediction.arraySync()[0]);
-//   // };
-
-//   return <> </>;
-// };
-
-// export default PredictionModel;
