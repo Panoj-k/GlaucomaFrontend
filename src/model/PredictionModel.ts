94e2a16dc24fc13dee5fc87c0f6ca528dc4bd458
Panoj Kamolrattanawech <panoj.ka@gmail.com>
2023-03-29 09:53:17 +0700

    add result from model to interface

--- src/model/PredictionModel.ts
+++ src/model/PredictionModel.ts
@@ -22,12 +22,84 @@ async function getPrediction() {
   console.log(predictionList);
 }
 
+// export async function PredictionModel(images: ImageInterface[]) {
+//   const model = await loadModel();
+//   imageList = [];
+//   const results: number[][] = [];
+
+//   // This one
+//   var bar = new Promise((resolve, reject) => {
+//     images.forEach((image, i) => {
+//       const imageUrl = image.url;
+//       const imageElement: HTMLImageElement | null = document.getElementById(
+//         `glaucomaImage${i}`
+//       ) as HTMLImageElement;
+//       const _imagefile = new Image();
+//       console.log("here");
+//       _imagefile.onload = async () => {
+//         console.log("here3");
+//         //console.log(model);
+//         //console.log(model.summary());
+//         if (imageElement && imageElement.src) {
+//           //@ts-ignore
+//           // imageElement.src = this.src
+//           const imageFile = cv.imread(imageElement);
+//           const dst: Mat = new cv.Mat(
+//             imageFile.cols,
+//             imageFile.rows,
+//             cv.CV_8UC4
+//           );
+//           cv.cvtColor(imageFile, dst, cv.COLOR_BGR2RGB);
+//           cv.resize(dst, dst, new cv.Size(224, 224), 0, 0, cv.INTER_AREA);
+//           cv.normalize(dst, dst, 0, 255, cv.NORM_MINMAX);
+
+//           console.log("here2");
+//           //console.log(dst.rows, dst.cols)
+//           const input = tf.tensor(
+//             dst.data,
+//             [1, dst.rows, dst.cols, 3],
+//             "float32"
+//           );
+//           //const input = tf.tensor(dst.data, [dst.rows, dst.cols, 3]);
+//           // console.log("line45");
+//           // imageList.push(input);
+//           //const imageTensor = tf.reshape()
+//           console.log(input);
+//           const prediction = model.predict(input) as tf.Tensor;
+//           console.log("prediction");
+//           console.log(prediction);
+//           const data = await prediction.data();
+//           console.log("data");
+//           console.log(data);
+//           console.log(data[0], data[1]);
+//           results.push([data[0], data[1]]);
+//           //console.log(model.output.shape); // e.g. [null, 10]
+//           //console.log(model.output.dtype); // e.g. "float32"
+//         }
+//       };
+//       _imagefile.src = imageElement.src;
+//       // if (i === images.length - 1) {
+//       //   getPrediction();
+//       // }
+//     });
+//   });
+//   return results;
+//   // bar.then(() => {
+//   //   console.log("All done!");
+//   //   console.log(imageList);
+//   //   const predictionList = model.predict(imageList);
+//   //   console.log(predictionList);
+//   // });
+// }
+
 export async function PredictionModel(images: ImageInterface[]) {
   const model = await loadModel();
   imageList = [];
+  const results: number[][] = [];
 
-  var bar = new Promise((resolve, reject) => {
-    images.forEach((image, i) => {
+  // This one
+  const promises = images.map((image, i) => {
+    return new Promise(async (resolve, reject) => {
       const imageUrl = image.url;
       const imageElement: HTMLImageElement | null = document.getElementById(
         `glaucomaImage${i}`
@@ -36,11 +108,7 @@ export async function PredictionModel(images: ImageInterface[]) {
       console.log("here");
       _imagefile.onload = async () => {
         console.log("here3");
-        console.log(model);
-        console.log(model.summary());
         if (imageElement && imageElement.src) {
-          //@ts-ignore
-          // imageElement.src = this.src
           const imageFile = cv.imread(imageElement);
           const dst: Mat = new cv.Mat(
             imageFile.cols,
@@ -50,41 +118,23 @@ export async function PredictionModel(images: ImageInterface[]) {
           cv.cvtColor(imageFile, dst, cv.COLOR_BGR2RGB);
           cv.resize(dst, dst, new cv.Size(224, 224), 0, 0, cv.INTER_AREA);
           cv.normalize(dst, dst, 0, 255, cv.NORM_MINMAX);
-
           console.log("here2");
-          //console.log(dst.rows, dst.cols)
           const input = tf.tensor(
             dst.data,
             [1, dst.rows, dst.cols, 3],
             "float32"
           );
-          //const input = tf.tensor(dst.data, [dst.rows, dst.cols, 3]);
-          // console.log("line45");
-          // imageList.push(input);
-          //const imageTensor = tf.reshape()
-          console.log(input);
           const prediction = model.predict(input) as tf.Tensor;
-          console.log("prediction");
-          console.log(prediction);
           const data = await prediction.data();
           console.log("data");
           console.log(data);
           console.log(data[0], data[1]);
-          const result = [data[0], data[1]];
-          //console.log(model.output.shape); // e.g. [null, 10]
-          //console.log(model.output.dtype); // e.g. "float32"
+          resolve([data[0], data[1]]);
         }
       };
       _imagefile.src = imageElement.src;
-      // if (i === images.length - 1) {
-      //   getPrediction();
-      // }
     });
   });
-  // bar.then(() => {
-  //   console.log("All done!");
-  //   console.log(imageList);
-  //   const predictionList = model.predict(imageList);
-  //   console.log(predictionList);
-  // });
+
+  return await Promise.all(promises);
 }
