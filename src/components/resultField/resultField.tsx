1d47f87a39bc73153e39ffc3fb2b0e7143443632
Panoj Kamolrattanawech <panoj.ka@gmail.com>
2023-04-04 15:27:09 +0700

    resize resultfield

--- src/components/resultField/resultField.tsx
+++ src/components/resultField/resultField.tsx
@@ -1,7 +1,9 @@
+import { useTranslation } from "react-i18next";
 import { useState } from "react";
 import { ImageInterface } from "../../interface/imageInterface";
 
 export default function ResultField({ images }: { images: ImageInterface[] }) {
+  const { t } = useTranslation();
   const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc");
 
   const sortedImages = images.sort((a, b) => {
@@ -22,10 +24,27 @@ export default function ResultField({ images }: { images: ImageInterface[] }) {
         Sort {sortOrder === "asc" ? "Descending" : "Ascending"}
       </button>
       <div>
-        {sortedImages.map((image) => (
-          <div key={image.name}>
-            <img src={image.url} alt={image.name} />
-            <p>Probability: {image.result[1]}</p>
+        {sortedImages.map((image, index) => (
+          <div
+            key={index}
+            style={{ width: "30%", padding: 5, position: "relative" }}
+          >
+            <img
+              id={`glaucomaImage${index}`}
+              src={image.url}
+              alt={`Uploaded picture ${index + 1}`}
+              style={{ width: "100%", height: "auto" }}
+            />
+            <p> {image.name}</p>
+            <p>
+              {t("result.Result")} {image.result[1]}
+              {"% "}
+            </p>
+            {/* <p> {t("result.ResultNormal")}</p> */}
+            <p>
+              {image.result[0] > image.result[1] && t("result.ResultNormal")}{" "}
+              {image.result[0] < image.result[1] && t("result.ResultGlaucoma")}
+            </p>
           </div>
         ))}
       </div>
